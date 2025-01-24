b71df8e133e9a8be2918af455241b38fb309bd1c
Riv_Falcon <26927165+RivFalcon@users.noreply.github.com>
2025-01-24 12:39:17 +0800

    Initiate Relic_Myth

---
+++ Relics/Relic_EN1.lua
@@ -0,0 +1,154 @@
+----
+--SMODS.Atlas{}
+
+SMODS.Joker{
+    key = "Relic_Calli",
+    talent = "Calli",
+    loc_txt = {
+        name = "Scythe of the Death Apprentice",
+        text = {
+            ''
+        }
+    },
+    config = { extra = {  } },
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+            }
+        }
+    end,
+    rarity = "hololive_Relic",
+    cost = 20,
+    blueprint_compat = true,
+
+    atlas = 'Relic_hololive',
+    pos = { x = 0, y = 0 },
+    soul_pos = { x = 0, y = 1 },
+
+    upgrade = function (self, card)
+    end,
+    calculate = function(self, card, context)
+    end
+}
+
+SMODS.Joker{
+    key = "Relic_Kiara",
+    talent = "Kiara",
+    loc_txt = {
+        name = "Flamming Sword of the Phoenix",
+        text = {
+            ''
+        }
+    },
+    config = { extra = {  } },
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+            }
+        }
+    end,
+    rarity = "hololive_Relic",
+    cost = 20,
+    blueprint_compat = true,
+
+    atlas = 'Relic_hololive',
+    pos = { x = 0, y = 0 },
+    soul_pos = { x = 0, y = 1 },
+
+    upgrade = function (self, card)
+    end,
+    calculate = function(self, card, context)
+    end
+}
+
+SMODS.Joker{
+    key = "Relic_Ina",
+    talent = "Ina",
+    loc_txt = {
+        name = "Ancient Tome of the Eldritch Priestess",
+        text = {
+            ''
+        }
+    },
+    config = { extra = {  } },
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+            }
+        }
+    end,
+    rarity = "hololive_Relic",
+    cost = 20,
+    blueprint_compat = true,
+
+    atlas = 'Relic_hololive',
+    pos = { x = 0, y = 0 },
+    soul_pos = { x = 0, y = 1 },
+
+    upgrade = function (self, card)
+    end,
+    calculate = function(self, card, context)
+    end
+}
+
+SMODS.Joker{
+    key = "Relic_Gura",
+    talent = "Gura",
+    loc_txt = {
+        name = "Trident of the Atlantic Shark",
+        text = {
+            ''
+        }
+    },
+    config = { extra = {  } },
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+            }
+        }
+    end,
+    rarity = "hololive_Relic",
+    cost = 20,
+    blueprint_compat = true,
+
+    atlas = 'Relic_hololive',
+    pos = { x = 0, y = 0 },
+    soul_pos = { x = 0, y = 1 },
+
+    upgrade = function (self, card)
+    end,
+    calculate = function(self, card, context)
+    end
+}
+
+SMODS.Joker{
+    key = "Relic_Ame",
+    talent = "Ame",
+    loc_txt = {
+        name = "Magnifying Glass of the Detective",
+        text = {
+            ''
+        }
+    },
+    config = { extra = {  } },
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+            }
+        }
+    end,
+    rarity = "hololive_Relic",
+    cost = 20,
+    blueprint_compat = true,
+
+    atlas = 'Relic_hololive',
+    pos = { x = 0, y = 0 },
+    soul_pos = { x = 0, y = 1 },
+
+    upgrade = function (self, card)
+    end,
+    calculate = function(self, card, context)
+    end
+}
+
+----
