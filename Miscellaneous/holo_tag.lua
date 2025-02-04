c6fa7bf1f00d24127140b48eaaaa9cb2a6f145b9
Riv_Falcon <26927165+RivFalcon@users.noreply.github.com>
2025-02-04 23:57:41 +0800

    Update holo_tag.lua

--- Miscellaneous/holo_tag.lua
+++ Miscellaneous/holo_tag.lua
@@ -1,11 +1,36 @@
 ----
 
+if false then
 SMODS.Tag{
     key = "Butterfly",
     loc_txt = {
-        ''
+        name = 'Butterfly Tag',
+        text = {
+            'Disables effect of',
+            'one {C:attention}Boss Blind{}',
+            'when entering it.'
+        }
     },
     
+    apply = function (self, tag, context)
+        print(context.setting_blind , G.GAME.blind.boss)
+        if context.setting_blind and G.GAME.blind.boss then
+            print(context.blind.disabled)
+            if context.blind.disabled == false then
+                tag:yep(
+                    localize('ph_boss_disabled'),
+                    G.C.GREEN,
+                    function()
+                        G.GAME.blind:disable()
+                        play_sound('timpani')
+                        return true
+                    end
+                )
+                tag.triggered = true
+            end
+        end
+    end
 }
+end
 
 ----
