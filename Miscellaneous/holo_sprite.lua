7e6b89987015b3ad1811fccf303d377715024d88
Riv_Falcon <26927165+RivFalcon@users.noreply.github.com>
2025-02-07 05:15:04 +0800

    Update holo_sprite.lua

--- Miscellaneous/holo_sprite.lua
+++ Miscellaneous/holo_sprite.lua
@@ -1,5 +1,16 @@
 ----
-
-
+SMODS.Atlas{
+    key = "Pebble",
+    path = "textures/Pebble.png",
+    px = 71,
+    py = 95
+}
+SMODS.Enhancement:take_ownership(
+    'm_stone',
+    {
+        atlas = "Pebble",
+        pos = {x=0,y=0}
+    }
+)
 
 ----
