c9458fda45394f48c4f6da1fe8a1be3361d5a619
Riv_Falcon <26927165+RivFalcon@users.noreply.github.com>
2025-02-09 15:06:32 +0800

    Moved the Observatory

--- Relics/Relic_EN2.lua
+++ Relics/Relic_EN2.lua
@@ -58,6 +58,14 @@ SMODS.Joker{ -- IRyS
     end
 }
 
+local LUH = level_up_hand
+function level_up_hand(card, hand, instant, amount)
+    LUH(card, hand, instant, amount)
+    for _,J in ipairs(G.jokers.cards) do
+        J:calculate_joker({level_up_hand = hand, level_up_amount = amount})
+    end
+end
+
 SMODS.Joker{ -- Tsukumo Sana
     key = "Relic_Sana",
     talent = "Sana",
