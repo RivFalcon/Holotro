0e9ede4b2f625783724c9391408d20774a65ebee
Riv_Falcon <26927165+RivFalcon@users.noreply.github.com>
2025-02-12 18:58:00 +0800

    Update en-us.lua

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,6 +1,173 @@
 return {
     descriptions={
         Other={
+            --- Gen Members ---
+            hololive_gen_origin={
+                name="Hololive 0th Generation",
+                text={
+                    "Tokino Sora",
+                    "Roboco-san",
+                    "Hoshimachi Suisei",
+                    "Sakura Miko",
+                    "AZKi"
+                }
+            },
+            hololive_gen_first={
+                name="Hololive 1st Generation",
+                text={
+                    "Yozora Mel",
+                    "Shiragami Fubuki",
+                    "Natsuiro Matsuri",
+                    "Aki Rosenthal",
+                    "Akai Haato"
+                }
+            },
+            hololive_gen_exodia={
+                name="Hololive 2nd Generation",
+                text={
+                    "Minato Aqua",
+                    "Murasaki Shion",
+                    "Nagiri Ayame",
+                    "Yuzuki Choco",
+                    "Oozora Subaru"
+                }
+            },
+            hololive_gen_gamers={
+                name="hololive Gamers",
+                text={
+                    "Shiragami Fubuki",
+                    "Ookami Mio",
+                    "Nekomata Okayu",
+                    "Inugami Korone"
+                }
+            },
+            hololive_gen_fantasy={
+                name="Hololive 3rd Generation: Fantasy",
+                text={
+                    "Usada Pekora",
+                    "{C:inactive}Uruha Rushia",
+                    "Shiranui Flare",
+                    "Shirogane Noel",
+                    "Houshou Marine"
+                }
+            },
+            hololive_gen_force={
+                name="Hololive 4th Generation",
+                text={
+                    "Amane Kanata",
+                    "Kiryu Coco",
+                    "Tsunomaki Watame",
+                    "Tokoyami Towa",
+                    "Himemori Luna"
+                }
+            },
+            hololive_gen_area15={
+                name="Hololive Indonesia 1st Generation -Area 15-",
+                text={
+                    "Ayunda Risu",
+                    "Moona Hoshinova",
+                    "Airani Iofifteen"
+                }
+            },
+            hololive_gen_nplab={
+                name="Hololive 5th Generation",
+                text={
+                    "Yukihana Lamy",
+                    "Momosuzu Nene",
+                    "Shishiro Botan",
+                    "Mano Aloe",
+                    "Omaru Polka"
+                }
+            },
+            hololive_gen_myth={
+                name="Hololive English -Myth-",
+                text={
+                    "Mori Calliope",
+                    "Takanashi Kiara",
+                    "Ninomae Ina'nis",
+                    "Gawr Gura",
+                    "Watson Amelia"
+                }
+            },
+            hololive_gen_holoro={
+                name="Hololive Indonesia 2nd Generation -Holoro-",
+                text={
+                    "Kureiji Ollie",
+                    "Anya Melfissa",
+                    "Pavolia Reine"
+                }
+            },
+            hololive_gen_promise={
+                name="Hololive English -Promise-",
+                text={
+                    "IRyS",
+                    "Tsukumo Sana",
+                    "Ceres Fauna",
+                    "Ouro Kronii",
+                    "Nanashi Mumei",
+                    "Hakos Baelz"
+                }
+            },
+            hololive_gen_holox={
+                name="Hololive 6th Generation: holoX",
+                text={
+                    "La+ Darkness",
+                    "Takane Lui",
+                    "Hakui Koyori",
+                    "Sakamata Chloe",
+                    "Kazama Iroha"
+                }
+            },
+            hololive_gen_holoh3ro={
+                name="Hololive Indonesia 3rd Generation -HoloH3ro-",
+                text={
+                    "Vestia Zeta",
+                    "Kaela Kovalskia",
+                    "Kobo Kanaeru"
+                }
+            },
+            hololive_gen_advent={
+                name="Hololive English -Advent-",
+                text={
+                    "Shiori Novella",
+                    "Koseki Bijou",
+                    "Nerissa Ravencroft",
+                    "Fuwawa Abyssgard",
+                    "Mococo Abyssgard"
+                }
+            },
+            hololive_gen_regloss={
+                name="Hololive DEV_IS: ReGLOSS",
+                text={
+                    "Hiodoshi Ao",
+                    "Otonose Kanade",
+                    "Ichijou Ririka",
+                    "Juufuutei Raden",
+                    "Todoroki Hajime"
+                }
+            },
+            hololive_gen_justice={
+                name="Hololive English -Justice-",
+                text={
+                    "Elizabeth Rose Bloodflame",
+                    "Gigi Murin",
+                    "Cecilia Immergreen",
+                    "Raora Panthera"
+                }
+            },
+            hololive_gen_flowglow={
+                name="Hololive DEV_IS: FLOW GLOW",
+                text={
+                    "Isaki Riona",
+                    "Koganei Niko",
+                    "Mizumiya Su",
+                    "Rindo Chihaya",
+                    "Kikirara Vivi"
+                }
+            },
+            ---
+
+            --- Info tips ---
             holo_info_even={
                 name="Even Cards",
                 text={
@@ -17,7 +184,40 @@ return {
     },
     misc={
         dictionary={
+            k_hololive_branch_jp="Hololive Japan",
+            k_hololive_branch_id="Hololive Indonesia",
+            k_hololive_branch_en="Hololive English",
+            k_hololive_branch_di="Hololive DEV_IS",
+            
+            k_hololive_gen_origin="Hololive 0th Generation",
+            k_hololive_gen_first="Hololive 1st Generation",
+            k_hololive_gen_exodia="Hololive 2nd Generation",
+            k_hololive_gen_gamers="hololive Gamers",
+            k_hololive_gen_fantasy="Hololive 3rd Generation: Hololive Fantasy",
+            k_hololive_gen_force="Hololive 4th Generation",
+            k_hololive_gen_area15="Hololive Indonesia 1st Generation -Area 15-",
+            k_hololive_gen_nplab="Hololive 5th Generation",
+            k_hololive_gen_myth="Hololive English -Myth-",
+            k_hololive_gen_holoro="Hololive Indonesia 2nd Generation -Holoro-",
+            k_hololive_gen_promise="Hololive English -Promise-",
+            k_hololive_gen_holox="Hololive 6th Generation: Secret Society holoX",
+            k_hololive_gen_holoh3ro="Hololive Indonesia 3rd Generation -HoloH3ro-",
+            k_hololive_gen_advent="Hololive English -Advent-",
+            k_hololive_gen_regloss="Hololive DEV_IS: ReGLOSS",
+            k_hololive_gen_justice="Hololive English -Justice-",
+            k_hololive_gen_flowglow="Hololive DEV_IS: FLOW GLOW",
+
+            k_hololive_fan="Fan",
+            k_hololive_mascot="Mascot",
+            k_hololive_meme="Meme",
+            k_hololive_relic="Relic",
+            k_hololive_song="Original Song",
+
             ph_hololive_necromancy="Saved by the Necromancer",
+        },
+        labels={
+            k_hololive_relic="Relic",
+
         }
     },
 }
