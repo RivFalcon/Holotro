e279c16472713ab5434427a20889799d6ec00917
Riv_Falcon <26927165+RivFalcon@users.noreply.github.com>
2025-02-18 23:24:05 +0800

    Adding Relic_Exodia

--- Miscellaneous/holo_sprite.lua
+++ Miscellaneous/holo_sprite.lua
@@ -13,4 +13,30 @@ SMODS.Enhancement:take_ownership(
     }
 )
 
+SMODS.Atlas{
+    key = "Lucky_Eye",
+    path = "textures/Lucky_Eye.png",
+    px = 71,
+    py = 95
+}
+SMODS.Enhancement:take_ownership(
+    'm_lucky',
+    {
+        config = {
+            mult=20, p_dollars = 20,
+            mult_odds=5, p_dollars_odds=15
+        },
+        loc_vars= function ()
+            return {
+                G.GAME.probabilities.normal,
+                card.ability.mult,
+                card.ability.mult_odds,
+                card.ability.p_dollars,
+                card.ability.p_dollars_odds
+            }
+        end,
+        atlas = "Lucky_Eye",
+        pos = {x=0,y=0}
+    }
+)
 ----
