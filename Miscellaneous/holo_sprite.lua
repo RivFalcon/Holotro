1d1b28375089dcc5c0446ffc9be094c506e8eeb0
Riv_Falcon <26927165+RivFalcon@users.noreply.github.com>
2025-02-19 11:06:09 +0800

    Fixed Lucky effect

--- Miscellaneous/holo_sprite.lua
+++ Miscellaneous/holo_sprite.lua
@@ -26,15 +26,6 @@ SMODS.Enhancement:take_ownership(
             mult=20, p_dollars = 20,
             mult_odds=5, p_dollars_odds=15
         },
-        loc_vars= function ()
-            return {
-                G.GAME.probabilities.normal,
-                card.ability.mult,
-                card.ability.mult_odds,
-                card.ability.p_dollars,
-                card.ability.p_dollars_odds
-            }
-        end,
         atlas = "Lucky_Eye",
         pos = {x=0,y=0}
     }
