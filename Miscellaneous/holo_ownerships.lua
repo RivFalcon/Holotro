3530a54fd6252e2dd9b47a7fb82080765c901ffa
Riv_Falcon <26927165+RivFalcon@users.noreply.github.com>
2025-03-09 20:06:18 +0800

    alpha_25w10g
    Hotfix

--- Miscellaneous/holo_ownerships.lua
+++ Miscellaneous/holo_ownerships.lua
@@ -12,11 +12,12 @@ SMODS.Enhancement:take_ownership(
     'm_lucky',
     {
         loc_vars = function(self, info_queue, card)
-            local is_eyed = next(find_joker('j_hololive_Relic_Shion')) and card:get_id()==14
+            local is_ace = (card and card.base and card.base.id or 0)==14 or false
+            local is_eyed = next(find_joker('j_hololive_Relic_Shion')) and is_ace
             return { vars = {
                 Holo.prob_norm(),
-                card.ability.extra.mult, is_eyed and 1 or 5,
-                card.ability.extra.p_dollars, is_eyed and 3 or 15,
+                card.ability.mult, is_eyed and 1 or 5,
+                card.ability.p_dollars, is_eyed and 3 or 15,
             } }
         end,
         atlas = "enhance_Lucky_Eye",
